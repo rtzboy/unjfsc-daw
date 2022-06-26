d959e1bb154c793878336fa446e27f563ccbcd56
jhoseph <japoma_200@hotmail.com>
2022-06-26 14:08:10 -0500

    point start

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+const App = () => <div>Helados</div>;
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
-// ----- REACT 18 -----
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
-
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-
-const app = <h1>jhosephPoma</h1>;
-
-const a = 2;
-if (a === 2) {
-	console.log('ga');
-}
+import App from './App';
+import './styles/index.css';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(app);
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
